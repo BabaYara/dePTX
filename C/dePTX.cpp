b93bb8faf2b2119e2b39748991f44edde4a9db99
evghenii <egaburov@dds.nl>
2013-12-31 21:44:39 +0100

    parse from stdin

--- C/dePTX.cpp
+++ C/dePTX.cpp
@@ -6,12 +6,16 @@ extern "C" FILE *yyin;
 
 int main() {
 	// open a file handle to a particular file:
-	FILE *myfile = fopen("kernel.ptx", "r");
+#if 0
+  FILE *myfile = fopen("kernel.ptx", "r");
 	// make sure it's valid:
 	if (!myfile) {
 		cout << "I can't open a.snazzle.file!" << endl;
 		return -1;
 	}
+#else
+  FILE *myfile = stdin;
+#endif
 	// set lex to read from it instead of defaulting to STDIN:
 	yyin = myfile;
 
