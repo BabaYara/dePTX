23565f86cb3667fbc9d8adaea0217d78a2f39045
evghenii <egaburov@dds.nl>
2014-01-01 15:10:13 +0100

    added compilation script

---
+++ compilePTX.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+PTXSRC=/tmp/___tmp_ptx.ptx
+PTXCU=/tmp/___tmp_ptx.cu
+PTXSH=/tmp/___tmp_ptx.sh
+
+
+NVCCPARM=${@:2}
+
+cp $1 $PTXSRC &&
+./dePTX < $PTXSRC > $PTXCU &&
+nvcc -arch=sm_35 -dc $NVCCPARM -dryrun $PTXCU 2>&1 | \
+  sed 's/\#\$//g'| \
+ awk '{ if ($1 == "cicc") print "cp /tmp/___tmp_ptx.ptx " $NF; else print $0 }' > $PTXSH
+sh $PTXSH
+
+# rm $PTXSRC $PTXCU $PTXSH
+
+
+
