7954b246e3a1f2e21c4ca123a0eb60a7c6d14ee9
evghenii <egaburov@dds.nl>
2014-01-01 16:31:26 +0100

    +1

--- compilePTX.sh
+++ compilePTX.sh
@@ -11,7 +11,7 @@ cp $1 $PTXSRC &&
 ./dePTX < $PTXSRC > $PTXCU &&
 nvcc -arch=sm_35 -dc $NVCCPARM -dryrun $PTXCU 2>&1 | \
   sed 's/\#\$//g'| \
- awk '{ if ($1 == "cicc") print "cp /tmp/___tmp_ptx.ptx " $NF; else print $0 }' > $PTXSH
+  awk '{ if ($1 == "LIBRARIES=") print $1$2; else if ($1 == "cicc") print "cp /tmp/___tmp_ptx.ptx " $NF; else print $0 }' > $PTXSH
 sh $PTXSH
 
 # rm $PTXSRC $PTXCU $PTXSH
